4c47ed2c9944e951c6a1aa15a9ba0ffa80ad8384
hesham1010 <152912294+hesham1010@users.noreply.github.com>
2023-12-22 02:48:08 +0500

    changes

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
-const MONGO_URL=
+const MONGO_URI=process.env.MONGODB_URI;
 let cached = (global as any).mongoose || {conn:null,promise:null}
 export const connectToDatabase=async()=>{
     if(cached.conn) return cached.connect;
-
+if(!MONGO_URI) throw new Error('MONGODB_URI is missing')
+cached.promise=cached.promise || mongoose.connect(`${MONGO_URI}`,{
+    dbName:"evently",
+    bufferCommands:false,
+})
+cached.conn=await cached.promise;
+return cached.conn
 } 
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,36 @@
+import { Schema,model ,models} from "mongoose";
+
+const userSchema=new Schema({
+ clerkId:{
+    type:String,
+    required:true,
+    unique:true,
+ },   
+ email:{
+    type:String,
+    required:true,
+    unique:true,
+ },   
+ username:{
+    type:String,
+    required:true,
+    unique:true,
+ },   
+ firstName:{
+    type:String,
+    required:true,
+   //  unique:true,
+ },   
+ lastName:{
+    type:String,
+    required:true,
+   //  unique:true,
+ },   
+ photo:{
+    type:String,
+    required:true,
+   //  unique:true,
+ },   
+})
+const User=models.User || model('User',userSchema);
+export default User;
